cbef433fa02f6f0e0e976512fd97ba94a222a00c
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2020-02-13 10:43:18 -0500

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -6,7 +6,7 @@ import time
 from ccc_table_parser import CCCTableParser
 from ccc_scraper import CCCScraper
 
-DEV_MODE = True
+DEV_MODE = False
 with open(".credentials", "r") as f:
     USERNAME, PASSWORD = f.read().split("\n")
 
